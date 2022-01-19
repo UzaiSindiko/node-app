7992b5a157ff7fc980cfeafddb3e42986d32f7f3
uzaisindiko <uzaisindiko@uzaisindikos-MacBook-Air.local>
2022-01-19 23:43:45 +0700

    .

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(
 app.use(express.json())
 
 app.get("/api/v1/", (req, res) => {
-  res.send("<h2>Hello World!</h2>")
+  res.send("<h2>Hello World!!!</h2>")
 })
 
 app.use("/api/v1/posts", postRouter)
